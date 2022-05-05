162e6ed5746ed0767efa3b762e507c31d152f979
weiteng <starincloud@yahoo.com.tw>
2022-05-06 00:44:46 +0800

    Update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,10 +12,12 @@ namespace Calculator
             ConsoleKeyInfo key;
 
             Console.WriteLine("Welcome to Calculator.");
+            Console.WriteLine("\nYour formula should consist only numbers, operators and/or brackets, and all shall be separated by spaces.");
 
             do
             {
-                string sum = UserInput();
+                Console.WriteLine("\nPlease key in your formula: ");
+                string sum = Console.ReadLine();
 
                 Regex regex = new Regex(@"^[0-9.*-/+() ]+$");
                 Match match = regex.Match(sum);
@@ -26,7 +28,15 @@ namespace Calculator
                 else
                 {
                     result = Calculate(sum);
-                    Console.WriteLine("\nResult: {0}", result);
+                    if (double.IsInfinity(result) || double.IsNaN(result) || double.IsNegativeInfinity(result) ||
+                    double.IsPositiveInfinity(result))
+                    {
+                        Console.WriteLine("\nResult is not valid.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nResult: {0}", Math.Round(result,2));
+                    }                    
                 }
                 Console.WriteLine("\nPress Esc to exit, any key to continue.");
                 key = Console.ReadKey();
@@ -42,9 +52,9 @@ namespace Calculator
         public static double Calculate(string sum)
         {
             List<string> formulaList = new List<string>();
-            string[] sumArray = sum.Split(' ');
+            string[] sumArray = sum.Trim().Split(' ');
 
-            foreach (var s in sumArray)
+            foreach (var s in sumArray) 
             {
                 formulaList.Add(s);
             }
@@ -62,42 +72,57 @@ namespace Calculator
         }
 
         /// <summary>
-        /// Do calculation without brackets, if there is  * /, do * / first.
+        /// Do calculation without brackets, do * / first, and follow by + -
         /// </summary>
         /// <param name="formulaList"></param>
         /// <returns></returns>
         private static double NormalCalculation(List<string> formulaList)
-        {          
-            for (int i = 0; i < formulaList.Count; i++)
+        {
+            try
             {
-                if (formulaList[i] == "*" || formulaList[i] == "/")
+                for (int i = 0; i < formulaList.Count; i++)
                 {
-                    result = DoOperatorsCalc(formulaList[i - 1], formulaList[i], formulaList[i + 1]);
-                    formulaList.RemoveRange(i - 1, 3);
-                    formulaList.Insert(i - 1, result.ToString());
-                    i = 0;
+                    if (formulaList[i] == "*" || formulaList[i] == "/")
+                    {
+                        result = DoOperatorsCalc(formulaList[i - 1], formulaList[i], formulaList[i + 1]);
+                        formulaList.RemoveRange(i - 1, 3);
+                        formulaList.Insert(i - 1, result.ToString());
+                        i = 0;
+                    }
+                }
+
+                for (int i = 0; i < formulaList.Count; i++)
+                {
+                    if (formulaList[i] == "+")
+                    {
+                        result = DoOperatorsCalc(formulaList[i - 1], formulaList[i], formulaList[i + 1]);
+                        formulaList.RemoveRange(i - 1, 3);
+                        formulaList.Insert(i - 1, result.ToString());
+                        i = 0;
+                    }
+                    if (formulaList[i] == "-")
+                    {
+                        result = DoOperatorsCalc(formulaList[i - 1], formulaList[i], formulaList[i + 1]);
+                        formulaList.RemoveRange(i - 1, 3);
+                        formulaList.Insert(i - 1, result.ToString());
+                        i = 0;
+                    }
+                }
+
+                if (formulaList.Count == 2)
+                {
+                    return double.NaN;
+                }
+                else
+                {
+                    return result;
                 }
             }
-
-            for (int i = 0; i < formulaList.Count; i++)
+            catch
             {
-                if (formulaList[i] == "+")
-                {
-                    result = DoOperatorsCalc(formulaList[i - 1], formulaList[i], formulaList[i + 1]);
-                    formulaList.RemoveRange(i - 1, 3);
-                    formulaList.Insert(i - 1, result.ToString());
-                    i = 0;
-                }
-                if (formulaList[i] == "-")
-                {
-                    result = DoOperatorsCalc(formulaList[i - 1], formulaList[i], formulaList[i + 1]);
-                    formulaList.RemoveRange(i - 1, 3);
-                    formulaList.Insert(i - 1, result.ToString());
-                    i = 0;
-                }
+                return double.NaN;
             }
-
-            return result;
+            
         }
 
         /// <summary>
@@ -125,91 +150,64 @@ namespace Calculator
                     case "-":
                         return d1 - d2;
                     default:
-                        throw new Exception("Operators not found.");
+                        return double.NaN;
                 }
             }
             catch
             {
-                Console.WriteLine("\nPlease key in valid formula.");
-                throw new Exception("\nPlease key in valid formula.");
+                return double.NaN;
             }            
         }
 
         /// <summary>
-        /// Formulate formula with brakets
+        /// Formulate formula with brackets
         /// </summary>
         /// <param name="formulaList"></param>
         /// <returns></returns>
         private static double BracketCalculation(List<string> formulaList)
         {
-            do
+            try
             {
-                int? closeBracket = null;
-                int? openBracket = null;
-
-                for (int i = 0; i < formulaList.Count; i++)
+                do
                 {
-                    if (formulaList[i].ToString() == "(")
-                    {
-                        openBracket = i;
-                    }
+                    int? closeBracket = null;
+                    int? openBracket = null;
 
-                    if (formulaList[i].ToString() == ")")
+                    for (int i = 0; i < formulaList.Count; i++)
                     {
-                        if (!closeBracket.HasValue)
+                        if (formulaList[i].ToString() == "(")
                         {
-                            closeBracket = i;
+                            openBracket = i;
+                        }
+
+                        if (formulaList[i].ToString() == ")")
+                        {
+                            if (!closeBracket.HasValue)
+                            {
+                                closeBracket = i;
+                            }
                         }
                     }
-                }
 
-                List<string> subFormula = formulaList.GetRange(openBracket.Value + 1, closeBracket.Value - openBracket.Value - 1);
-                if (subFormula.Count < 3)
-                {
-                    throw new Exception("Please key in valid formula.");
-                }
-                result = NormalCalculation(subFormula);
-                formulaList.RemoveRange(openBracket.Value, closeBracket.Value - openBracket.Value + 1);
-                formulaList.Insert(openBracket.Value, result.ToString());
-            } while (formulaList.Contains("("));
+                    List<string> subFormula = formulaList.GetRange(openBracket.Value + 1, closeBracket.Value - openBracket.Value - 1);
+                    if (subFormula.Count < 3)
+                    {
+                        return result = double.NaN;
+                    }
+                    result = NormalCalculation(subFormula);
+                    formulaList.RemoveRange(openBracket.Value, closeBracket.Value - openBracket.Value + 1);
+                    formulaList.Insert(openBracket.Value, result.ToString());
+                } while (formulaList.Contains("("));
 
-            result = NormalCalculation(formulaList);
-
-            return result;
-        }
-
-        /// <summary>
-        /// Get user input
-        /// </summary>
-        /// <returns></returns>
-        private static string UserInput()
-        {
-           
-            Console.WriteLine("\nPlease key in your formula: ");
-            string sum = Console.ReadLine();
-            //Regex regex = new Regex(@"1234567890\+-\*/\.\(\)");
-            //Match match = regex.Match(sum);
-            //if (match.Success)
-            //{
-            //    Console.WriteLine("nmatch");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Please key in valid formula.");
                 
+                result = NormalCalculation(formulaList);
 
-            //}
-
-            return sum;
-
-        }
-
-        static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
-        {
-            Console.WriteLine(e.ExceptionObject.ToString());
-            Console.WriteLine("Press Enter to Exit");
-            Console.ReadLine();
-            Environment.Exit(0);
-        }
+                return result;
+            }
+            catch
+            {
+                return result = double.NaN;
+            }
+        }       
     }
 }
